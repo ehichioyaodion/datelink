b9df2b06a124bae958aaee4a28fb8480919126c6
ehichioyaodion <odionehichioya@gmail.com>
2025-03-15 21:35:05 +0000

    updates

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
-// eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
-config.resolver.assetExts.push('cjs');
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = config;
